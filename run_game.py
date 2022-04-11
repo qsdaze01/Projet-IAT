6667446b600f086fe9488ce769bbac6bdf4e2694
gaspard <gaspard.michel@insa-lyon.fr>
2022-04-11 08:38:12 +0200

    ajout select_action

--- run_game.py
+++ run_game.py
@@ -2,12 +2,14 @@ from time import sleep
 from game.SpaceInvaders import SpaceInvaders
 from controller.keyboard import KeyboardController
 from controller.random_agent import RandomAgent
+from controller.IA_agent import IA_agent
 
 def main():
 
     game = SpaceInvaders(display=True)
-    controller = KeyboardController()
+    #controller = KeyboardController()
     #controller = RandomAgent(game.na)
+    controller = IA_agent()
     game.get_state()
     state = game.reset()
     while True:
